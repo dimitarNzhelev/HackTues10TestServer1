b80ab11e20d928cbd1814ffb7703af9587ba9097
Dimitar Zhelev <80209250+dimitarNzhelev@users.noreply.github.com>
2023-06-29 07:17:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const saveRouter = require("./routes/save");
 
 initializePassport(passport);
 
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
