b9e9049bd1804c70430c161de0fea77cb9ca2a1d
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-06-29 20:00:44 +0300

    add cors to homejs, idk

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { getUserById } = require("../controllers/userController");
+const cors = require("cors");
+
+const allowedOrigin = "https://jellyfish-app-5kx28.ondigitalocean.app";
+
+router.use(
+  cors({
+    origin: allowedOrigin,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 router.get("/", async (req, res) => {
   console.log(req.session);
